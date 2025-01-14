7ece4268a06a10e12c01895cbff82d63d831df57
alvaro bernucci <alvarosenabernucci@hotmail.com>
2025-01-14 14:47:35 -0300

    Finalização ta todo-list

--- api/tasks/views.py
+++ api/tasks/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
 from tasks import models, serializers
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = serializers.TaskSerializer
+    permission_classes = [IsAuthenticated]
+
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
